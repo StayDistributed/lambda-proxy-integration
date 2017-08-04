1254a3cea5c725a435f0d845b4b5d2182b000676
Michele Salvini <michele@scozzese.com>
2017-08-04 11:48:50 +0200

    createIntegration

---
+++ index.js
@@ -0,0 +1,116 @@
+var amazonApiGatewayQuerystring = require('amazon-api-gateway-querystring');
+
+module.exports = function (configFactory) {
+
+  return function (event, context, callback) {
+
+    var output = {
+          statusCode: 200,
+          headers: {},
+          body: {}
+        },
+        requestPayload = {
+          headers: event.headers,
+          accessToken: event.headers && event.headers.Authorization,
+          identity: event && event.requestContext && event.requestContext.authorizer && event.requestContext.authorizer.claims || null,
+          sourceIP: event && event.requestContext && event.requestContext.identity && event.requestContext.identity.sourceIp || null,
+          stageVariables: event.stageVariables || {},
+          path: event.path || '',
+          resource: event.resource || '/',
+          httpMethod: event.httpMethod || 'DEFAULT',
+          origin: event.headers && [event.headers['X-Forwarded-Proto'], event.headers.Host].join('://') || '',
+          stage: (event.requestContext && event.requestContext.stage || ''),
+          pathParameters: Object.keys(event.pathParameters || {}).reduce(function (params, k) { params[k] = decodeURIComponent(event.pathParameters[k]); return params; }, {}),
+          queryStringParameters: event.queryStringParameters || {},
+          body: event.body || {}
+        };
+
+    var _config = Object.assign(
+                    {
+                      // base directory where start to map resource_path to files
+                      methodsBaseDir: 'resources',
+
+                      // parse querystring looking for nested params
+                      // https://www.npmjs.com/package/amazon-api-gateway-querystring
+                      parseQueryString: true,
+
+                      // parse event.body looking for JSON structure
+                      parseBody: true,
+
+                      // function called with response that returns the output object
+                      response: function (response) {
+                        return {
+                          body: response
+                        };
+                      },
+
+                      // function called with error that returns the output object
+                      error: function (error) {
+                        return {
+                          statusCode: 500,
+                          body: {
+                            error: error
+                          }
+                        };
+                      }
+
+                    },
+                    configFactory({requestPayload})
+                  );
+
+
+    if (_config.parseQueryString && Object.keys(requestPayload.queryStringParameters).length)
+      requestPayload.queryStringParameters = amazonApiGatewayQuerystring(requestPayload.queryStringParameters);
+
+
+    if (_config.parseBody && event.body) {
+      try {
+        var parsed = JSON.parse(event.body);
+        if (parsed)
+          requestPayload.body = parsed;
+      } catch (e) {}
+    }
+
+
+    var moduleName = requestPayload.resource + '/' + requestPayload.httpMethod.toUpperCase();
+    console.log(moduleName)
+
+    try {
+      var module = require('./' + _config.methodsBaseDir + moduleName);
+    }
+    catch (error) {
+
+      output = _config.error({
+        code: 403,
+        message: "The resource path doesn\'t exists"
+      });
+
+      if (output.body)
+        output.body = JSON.stringify(output.body);
+      callback(null, output);
+
+      return;
+    }
+
+    module(requestPayload).then(function (response) {
+
+      output = _config.response(response);
+
+      if (output.body)
+        output.body = JSON.stringify(output.body);
+      callback(null, output);
+
+    }).catch(function (error) {
+
+      output = _config.error(error);
+
+      if (output.body)
+        output.body = JSON.stringify(output.body);
+      callback(null, output);
+
+    });
+
+  }
+
+}
+
